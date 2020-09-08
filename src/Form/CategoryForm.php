64727df2ec31a4af1bc3fd49452dba1bce6abb76
Kevin Lessing <k.lessing@loom.de>
2020-09-08 15:58:33 +0200

    Descriptions

--- src/Form/CategoryForm.php
+++ src/Form/CategoryForm.php
@@ -46,7 +46,7 @@ class CategoryForm extends EntityForm {
       '#title' => $this->t('Label'),
       '#maxlength' => 255,
       '#default_value' => $category->label(),
-      '#description' => $this->t('Label for the category.'),
+      '#description' => 'Label for the category.',
       '#required' => TRUE,
     ];
 
@@ -67,113 +67,239 @@ class CategoryForm extends EntityForm {
 
     $form['detailedDescription'] = [
       '#type' => 'text_format',
-      '#title' => $this->t('Detailed description'),
+      '#title' => 'Detailierte Beschreibung',
       '#default_value' => $category->detailedDescription['value'],
-      '#description' => $this->t('This can be used to describe the cookies and scripts belonging to the category.'),
+      '#description' => 'Kann genutzt werden, um die zu dieser Kategorie gehörenden Cookies und Skripte näher zu beschreiben.',
     ];
 
-    $form['whitelist'] = [
+    $form['cookies_section'] = [
       '#type' => 'fieldset',
-      '#title' => t('Whitelist'),
-      '#open' => TRUE,
+      '#title' => 'Cookies',
       0 => [
-        '#markup' => $this->t(
-          'These cookies and scripts will be blocked unless the user ' .
-          'accepts this category.' .
-          '<p><strong>Scripts</strong> can be blocked server-side or client-side. ' .
-          'It is recommended to prefer server-side blocking due to performance ' .
-          'reasons. Some scripts cannot be blocked server-side (e.g. scripts ' .
-          'loaded by Google Tag Manager), thus you will have to use client-side ' .
-          'blocking for them.</p>' .
-          '<p><strong>Cookies</strong> can only be blocked client-side. ' .
-          'However, this module cannot prevent cookies from beeing set at all. ' .
-          'The cookies are deleted in an interval of 5 seconds. So a user could ' .
-          'be tracked nevertheless. Therefore it is recommended that you instead ' .
-          'block the scripts that would set the cookies, if possible.</p>'
-        ),
+        '#markup' =>
+          'Von vornherein werden erst einmal alle Cookies geblockt (Ausnahme: ' .
+          'Cookies, die von Drupal benötigt werden). Erst wenn der Nutzer eine ' .
+          'oder mehrere Kategorien über den Cookiebanner aktiviert, werden die ' .
+          'angegebenen Cookies aktiviert.<br/>' .
+          'Das Blocken der Cookies erfolgt clientseitig. Dabei werden alle paar ' .
+          'Sekunden alle Cookies, die nicht per Kategorie zugelassen werden ' .
+          'sollen, gelöscht. Dadurch kann es trotzdem vorkommen, dass der ' .
+          'Nutzer getrackt wird, weil die Cookies für eine gewisse Zeit gesetzt ' .
+          'sein können. Deshalb wird empfohlen, zusätzlich diejenigen Skripte zu ' .
+          'blockieren, welche diese Cookies setzen. Siehe Abschnitt ' .
+          '<a href="#edit-scripts-section">Skripte</a>.',
       ],
     ];
 
-    $form['whitelist']['server_side'] = [
-      '#type' => 'details',
-      '#title' => t('Server Side'),
-      '#open' => TRUE,
-    ];
-
-    $form['whitelist']['server_side']['attachmentNames'] = [
+    $form['cookies_section']['cookies'] = [
       '#type' => 'textarea',
-      '#title' => $this->t('Attachment names'),
-      '#default_value' => implode("\n", $category->attachmentNames),
-      '#description' => $this->t(
-        'Names of Drupal attachments. If possible, you should prefer this ' .
-        'method over the others due to performance reasons. You can determine ' .
-        'attachments by reading the source code of modules or using a debugger.<br/>' .
-        'One attachment name per line.<br/>' .
-        'Example:<br/>' .
-        'matomo_tracking_script'),
-    ];
-
-    $form['whitelist']['server_side']['scriptUrlRegexes'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Script URL regexes'),
-      '#description' => $this->t(
-        'One regex per line.' . '<br/>' .
-        'Example:<br/>' .
-        '.*google-analytics\.com/analytics\.js'
-      ),
-      '#default_value' => implode("\n", $category->scriptUrlRegexes),
-    ];
-
-    $form['whitelist']['server_side']['scriptBlockRegexes'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Script Block regexes'),
-      '#description' => $this->t(
-        'Use this to block specific script blocks by comparing their
-        content with a regular expression<br/>' .
-        'One regex per line.' . '<br/>' .
-        'Example:<br/>' .
-        '.*document\.cookie=.*'
-      ),
-      '#default_value' => implode("\n", $category->scriptBlockRegexes),
-    ];
-
-    $form['whitelist']['server_side']['embedUrlRegexes'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Embed URL regexes (embeds + iframes)'),
-      '#description' => $this->t(
-        'One regex per line.<br/>' .
-        'Example:<br/>' .
-        'https://www\.google\.com/maps/embed\?.*'
-      ),
-      '#default_value' => implode("\n", $category->embedUrlRegexes),
-    ];
-
-    $form['whitelist']['client_side'] = [
-      '#type' => 'details',
-      '#title' => t('Client Side'),
-      '#open' => TRUE,
-    ];
-
-    $form['whitelist']['client_side']['cookies'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Cookie names'),
       '#default_value' => implode("\n", $category->cookies),
-      '#description' => $this->t(
-        'One cookie name per line.'
-      )
+      '#description_display' => 'before',
+      '#description' => [
+        [
+          '#markup' => 'Alle Cookies, die zu dieser Kategorie gehören, müssen hier angegeben werden. Wenn der Nutzer ' .
+            'seine Auswahl im Cookiebanner trifft, dann werden nur die Cookies der ' .
+            'ausgewählten Kategorien aktiviert. Alle anderen werden gelöscht.<br/>' .
+            'Ein Cookiename pro Zeile.<br/>',
+        ],
+        [
+          '#type' => 'details',
+          '#title' => 'Beispiele',
+          0 => [
+            '#markup' =>
+              'SID<br/>' .
+              'SSID<br/>' .
+              'APISID<br/>' .
+              'IDE',
+          ],
+        ],
+      ],
     ];
 
-    $form['whitelist']['client_side']['scriptUrlRegexesClientSide'] = [
+    $form['scripts_section'] = [
+      '#type' => 'fieldset',
+      '#title' => 'Skripte',
+      0 => [
+        '#markup' =>
+          'Skripte können serverseitig und clientseitig geblockt werden. Aus ' .
+          'Performancegründen sollte bevorzugt das serverseitige Blockieren ' .
+          'genutzt werden, sofern das möglich ist. Einige Skripte lassen sich allerdings ' .
+          'nur clientseitig blockieren. Dazu gehören beispielsweise per Skripte, ' .
+          'die per Google Tag Manager nachgeladen werden.',
+      ],
+    ];
+
+    $form['scripts_section']['examples'] = [
+      '#type' => 'details',
+      '#title' => 'Beispiele',
+      '#open' => FALSE,
+      0 => [
+        '#markup' =>
+          '<li>Skripte, die per Google Tag Manager nachgeladen werden (bspw. <strong>Google Analytics</strong>): <a href="#edit-scripturlregexesclientside">clientseitig</a></li>' .
+          '<li>' .
+          'Skripte, die von Drupal eingebunden werden:' .
+          '<ul>' .
+          '<li>per Attachment (bspw. <strong>Matomo</strong>): <a href="#edit-attachmentnames">serverseitig &rarr; Attachment</a></li>' .
+          '<li>' .
+          'als &lt;script&gt;-Tag (bspw. per Template)' .
+          '<ul>' .
+          '<li>mit src: <a href="#edit-scripturlregexes">serverseitig &rarr; Script URL regexes</a></li>' .
+          '<li>als Script-Block: <a href="#edit-scriptblockregexes">serverseitig &rarr; Script Block regexes</a></li>' .
+          '</ul>' .
+          '</li>' .
+          '</ul>' .
+          '</li>' .
+          '</ul>',
+      ],
+    ];
+
+    $form['scripts_section']['server_side'] = [
+      '#type' => 'details',
+      '#title' => t('Serverseitiges Blockieren'),
+      '#open' => FALSE,
+    ];
+
+    $form['scripts_section']['server_side']['attachmentNames'] = [
       '#type' => 'textarea',
-      '#title' => $this->t('Script URL regexes (blocked client-side)'),
-      '#description' => $this->t(
-        'One regex per line.<br/>' .
-        'Use this option for scripts that are loaded by JS and therefore cannot' .
-        ' be blocked on server side.' . '<br/>' .
-        'Example:<br/>' .
-        '.*google-analytics\.com/analytics\.js'
-      ),
+      '#title' => 'Attachments',
+      '#default_value' => implode("\n", $category->attachmentNames),
+      '#description_display' => 'before',
+      '#description' =>
+        [
+          [
+            '#markup' => 'Namen von Drupal-Attachments. Aus Performancegründen sollte diese ' .
+              'Methode bevorzugt werden, wenn möglich. Namen von Drupal-Attachments ' .
+              'können durch Lesen der Modulquelltexte oder durch Debugging ' .
+              'herausgefunden werden.<br/>' .
+              'Ein Attachment pro Zeile.<br/>',
+          ],
+          [
+            '#type' => 'details',
+            '#title' => 'Beispiele',
+            '#open' => FALSE,
+            0 => [
+              '#markup' =>
+                '<strong>Matomo:</strong><br/>' .
+                'matomo_tracking_script',
+            ],
+          ],
+        ],
+    ];
+
+    $form['scripts_section']['server_side']['scriptUrlRegexes'] = [
+      '#type' => 'textarea',
+      '#title' => 'Script-URLs',
+      '#default_value' => implode("\n", $category->scriptUrlRegexes),
+      '#description_display' => 'before',
+      '#description' =>
+        [
+          [
+            '#markup' => 'Ein Regex pro Zeile.',
+          ],
+          [
+            '#type' => 'details',
+            '#title' => 'Beispiele',
+            '#open' => FALSE,
+            0 => [
+              '#markup' =>
+                '<strong>Google Analytics (sofern es serverseitig eingebunden wird):</strong><br/>' .
+                '.*google-analytics\.com/analytics\.js',
+            ],
+          ],
+        ],
+    ];
+
+    $form['scripts_section']['server_side']['scriptBlockRegexes'] = [
+      '#type' => 'textarea',
+      '#title' => 'Inline-Script-Blöcke',
+      '#default_value' => implode("\n", $category->scriptBlockRegexes),
+      '#description_display' => 'before',
+      '#description' =>
+        [
+          [
+            '#markup' =>
+              'Diese regulären Ausdrücke werden genutzt, um Inline-Script-Blöcke ' .
+              'zu blockieren<br/>' .
+              'Ein Regex pro Zeile.',
+          ],
+          [
+            '#type' => 'details',
+            '#title' => 'Beispiele',
+            '#open' => FALSE,
+            0 => [
+              '#markup' =>
+                '.*document\.cookie=.*',
+            ],
+          ],
+        ],
+    ];
+
+    $form['scripts_section']['client_side'] = [
+      '#type' => 'details',
+      '#title' => t('Clientseitiges Blockieren'),
+      '#open' => FALSE,
+    ];
+
+    $form['scripts_section']['client_side']['scriptUrlRegexesClientSide'] = [
+      '#type' => 'textarea',
+      '#title' => 'Script-URLs',
       '#default_value' => implode("\n", $category->scriptUrlRegexesClientSide),
+      '#description_display' => 'before',
+      '#description' =>
+        [
+          [
+            '#markup' =>
+              'Für Skripte, die nicht serverseitig geblockt werden können.<br/>' .
+              'Ein Regex pro Zeile.',
+          ],
+          [
+            '#type' => 'details',
+            '#title' => 'Beispiele',
+            '#open' => FALSE,
+            0 => [
+              '#markup' =>
+                '<strong>Google Analytics (sofern es per Google Tag Manager eingebunden wird):</strong><br/>' .
+                '.*google-analytics\.com/analytics\.js',
+            ],
+          ],
+        ],
+    ];
+
+    $form['embeds'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Embeds + iFrames'),
+      0 => [
+        '#markup' => 'Embed und iFrames werden serverseitig blockiert. Sie ' .
+          'werden nachgeladen, wenn der Nutzer die entsprechene Kategorie im ' .
+          'Cookiebanner aktiviert.',
+      ],
+    ];
+
+    $form['embeds']['embedUrlRegexes'] = [
+      '#type' => 'textarea',
+      '#title' => 'Embed-URLs (Embeds + iFrames)',
+      '#default_value' => implode("\n", $category->embedUrlRegexes),
+      '#description_display' => 'before',
+      '#description' =>
+        [
+          [
+            '#markup' =>
+              'Ein Regex pro Zeile.',
+          ],
+          [
+            '#type' => 'details',
+            '#title' => 'Beispiele',
+            '#open' => FALSE,
+            0 => [
+              '#markup' =>
+                '<strong>Google Maps:</strong><br/>' .
+                'https://www\.google\.com/maps/embed\?.*' .
+                '<br/><br/>' .
+                '<strong>Youtube:</strong><br/>' .
+                'https://www\.youtube\.com/.*',
+            ],
+          ],
+        ],
     ];
 
     return $form;
